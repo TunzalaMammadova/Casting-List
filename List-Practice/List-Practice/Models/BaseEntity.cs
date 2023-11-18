764f2e406f0ecb34da65f70b71247661d4453543
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2023-11-18 06:58:46 +0400

    added new file

--- List-Practice/List-Practice/Models/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace List_Practice.Models
-{
-	public abstract class BaseEntity
-	{
-		public int Id { get; set; }
-	}
-}
-
--- List-Practice/List-Practice/Models/Shark.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace List_Practice.Models
-{
-	public class Shark :Animal
-	{
-		public int Age { get; set; }
-	}
-}
-
